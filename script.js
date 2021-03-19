aa78d930d81b683a57e6d11a30253ac4d256b379
Askhab <asmag86@gmail.com>
2021-03-19 15:14:31 +0300

    Added some more fetch examples

--- script.js
+++ script.js
@@ -66,3 +66,54 @@ window.onload = function () {
 
 //     setTimeout(() => img.remove(), 3000); // (*)
 //   });
+
+// // Другая реализация
+// fetch('/article/promise-chaining/user.json')
+//   .then(response => response.json())
+//   .then(user => fetch(`https://api.github.com/users/${user.name}`))
+//   .then(response => response.json())
+//   .then(githubUser => new Promise(function(resolve, reject) { // (*)
+//     let img = document.createElement('img');
+//     img.src = githubUser.avatar_url;
+//     img.className = "promise-avatar-example";
+//     document.body.append(img);
+
+//     setTimeout(() => {
+//       img.remove();
+//       resolve(githubUser); // (**)
+//     }, 3000);
+//   }))
+//   // срабатывает через 3 секунды
+//   .then(githubUser => alert(`Закончили показ ${githubUser.name}`));
+
+// // Третья реализация
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => response.json());
+// }
+
+// function loadGithubUser(name) {
+//   return fetch(`https://api.github.com/users/${name}`)
+//     .then(response => response.json());
+// }
+
+// function showAvatar(githubUser) {
+//   return new Promise(function(resolve, reject) {
+//     let img = document.createElement('img');
+//     img.src = githubUser.avatar_url;
+//     img.className = "promise-avatar-example";
+//     document.body.append(img);
+
+//     setTimeout(() => {
+//       img.remove();
+//       resolve(githubUser);
+//     }, 3000);
+//   });
+// }
+
+// // Используем их:
+// loadJson('/article/promise-chaining/user.json')
+//   .then(user => loadGithubUser(user.name))
+//   .then(showAvatar)
+//   .then(githubUser => alert(`Показ аватара ${githubUser.name} завершён`));
+//   // ...
