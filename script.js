8a352c7cc430efd8e080b1df87ccaf83e90deb58
Askhab <asmag86@gmail.com>
2021-03-25 16:51:15 +0300

    Work started

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
 
+window.onload = function() {
+    const api = "https://swapi.dev/api/";
+    let objectType = document.querySelector('option[selected]'),
+        nameOutput = document.querySelector('#name'),
+        heightOutput = document.querySelector('#height'),
+        massOutput = document.querySelector('#mass'),
+        birthYearOutput = document.querySelector('#birth_year'),
+        filmsCountOutput = document.querySelector('#films_count');
+
+    console.log(objectType.value);
+};
