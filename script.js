acb0f40c742118097586be8f4cc6dcc231cf2b6d
Askhab <asmag86@gmail.com>
2021-03-30 11:00:48 +0300

    Added info about fetch

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-window.onload = function() {
-    const api = "https://swapi.dev/api/";
+window.onload = async function() {
+    const api = "https://swapi.dev/api",
+          searchButton = document.querySelector('#search_request_btn');
+
     let objectType = document.querySelector('option[selected]'),
         nameOutput = document.querySelector('#name'),
         heightOutput = document.querySelector('#height'),
@@ -9,5 +11,14 @@ window.onload = function() {
         birthYearOutput = document.querySelector('#birth_year'),
         filmsCountOutput = document.querySelector('#films_count');
 
-    console.log(objectType.value);
+    // let response = await fetch(api, {
+    //     method: 'GET',
+    // });
+       
+    // let result = await response.json();
+
+    fetch(api)
+      .then(response => response.json())
+      .then(response => console.log(response));
+
 };
