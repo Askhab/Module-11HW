b35094ca466984a8ce496598271bb5c29edc1d81
Askhab <asmag86@gmail.com>
2021-03-31 12:30:31 +0300

    Request function

--- fetch.js
+++ fetch.js
@@ -1,29 +1,29 @@
-"use strict";
+// "use strict";
 
-document.onload = function() {
-    const api = "https://swapi.dev/api",
-          searchButton = document.querySelector('#search_request_btn');
+// document.onload = function() {
+//     const api = "https://swapi.dev/api",
+//           searchButton = document.querySelector('#search_request_btn');
 
-    let object = document.querySelector('.person_search'),
-        objectType = document.querySelector('option[selected]'),
-        findedObjects = document.querySelector('.search_result'),
-        nameOutput = document.querySelector('#name'),
-        heightOutput = document.querySelector('#height'),
-        massOutput = document.querySelector('#mass'),
-        birthYearOutput = document.querySelector('#birth_year'),
-        filmsCountOutput = document.querySelector('#films_count'),
-        xhrRequest = new XMLHttpRequest();
-
-    
-    searchButton.addEventListener('click', );
+//     let object = document.querySelector('.person_search'),
+//         objectType = document.querySelector('option[selected]'),
+//         findedObjects = document.querySelector('.search_result'),
+//         nameOutput = document.querySelector('#name'),
+//         heightOutput = document.querySelector('#height'),
+//         massOutput = document.querySelector('#mass'),
+//         birthYearOutput = document.querySelector('#birth_year'),
+//         filmsCountOutput = document.querySelector('#films_count'),
+//         xhrRequest = new XMLHttpRequest();
 
     
-    xhrRequest.open('GET', api, true);
-    xhrRequest.responseType = 'json';
-    xhrRequest.onload = () => {
-        console.log(xhrRequest.response);
-    };
-    xhrRequest.send();
+//     searchButton.addEventListener('click', );
+
+    
+//     xhrRequest.open('GET', api, true);
+//     xhrRequest.responseType = 'json';
+//     xhrRequest.onload = () => {
+//         console.log(xhrRequest.response);
+//     };
+//     xhrRequest.send();
 
 
-};
+// };
--- xhr.js
+++ xhr.js
@@ -1,29 +1,39 @@
 "use strict";
 
-document.onload = function() {
-    const api = "https://swapi.dev/api",
-          searchButton = document.querySelector('#search_request_btn');
 
-    let object = document.querySelector('.person_search'),
-        objectType = document.querySelector('option[selected]'),
-        findedObjects = document.querySelector('.search_result'),
-        nameOutput = document.querySelector('#name'),
-        heightOutput = document.querySelector('#height'),
-        massOutput = document.querySelector('#mass'),
-        birthYearOutput = document.querySelector('#birth_year'),
-        filmsCountOutput = document.querySelector('#films_count'),
-        xhrRequest = new XMLHttpRequest();
+const api = "https://swapi.dev/api/",
+        searchButton = document.querySelector('#search_request_btn');
 
-    
-    searchButton.addEventListener('click', );
+let object = document.querySelector('.person_search'),
+    objectType = document.querySelector('.search_type'),
+    findedObjects = document.querySelector('.search_result'),
+    nameOutput = document.querySelector('#name'),
+    heightOutput = document.querySelector('#height'),
+    massOutput = document.querySelector('#mass'),
+    birthYearOutput = document.querySelector('#birth_year'),
+    filmsCountOutput = document.querySelector('#films_count');
 
-    
-    xhrRequest.open('GET', api, true);
-    xhrRequest.responseType = 'json';
-    xhrRequest.onload = () => {
-        console.log(xhrRequest.response);
+
+searchButton.addEventListener('click', requestData);
+
+
+// function getData() {
+//     requestData();
+
+// }
+
+function requestData() {
+    const xhr = new XMLHttpRequest(),
+          url = api + objectType.value;
+
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        let data = xhr.response;
+        console.log(data);
+        return data;
     };
-    xhrRequest.send();
+    xhr.send();
 
+}
 
-};
