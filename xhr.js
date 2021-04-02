aa23c8ac988644d3f61c399e3123ec5f06a5d362
Askhab <asmag86@gmail.com>
2021-04-02 15:08:41 +0300

    Commit #4

--- xhr.js
+++ xhr.js
@@ -1,29 +1,28 @@
 "use strict";
 
-
 const api = "https://swapi.dev/api/",
-      searchButton = document.querySelector('#search_request_btn');
+      searchButton = document.querySelector("#search_request_btn");
 
-let object = document.querySelector('.person_search'),
-    objectType = document.querySelector('.search_type'),
-    findedObjects = document.querySelector('.search_result'),
-    nameOutput = document.querySelector('#name'),
-    heightOutput = document.querySelector('#height'),
-    massOutput = document.querySelector('#mass'),
-    birthYearOutput = document.querySelector('#birth_year'),
-    filmsCountOutput = document.querySelector('#films_count');
+let object = document.querySelector(".person_search"),
+    objectType = document.querySelector(".search_type"),
+    findedObjects = document.querySelector(".search_result"),
+    nameOutput = document.querySelector("#name"),
+    heightOutput = document.querySelector("#height"),
+    massOutput = document.querySelector("#mass"),
+    birthYearOutput = document.querySelector("#birth_year"),
+    filmsCountOutput = document.querySelector("#films_count");
 
-
-searchButton.addEventListener('click', requestData);
-
-
-// function getData() {
-//     requestData();
-
-// }
+searchButton.addEventListener("click", requestData);
 
 function requestData() {
-    return new Promise((resolve, reject) => {
-        const xhr = XMLHttpRequest();
-    });
+  const xhr = new XMLHttpRequest(),
+        url = api + objectType.value;
+
+  xhr.open("GET", url);
+  xhr.responseType = "json";
+  xhr.send();
+  xhr.onload = () => {
+    let object = xhr.response;
+    console.log(object);
+  };
 }
