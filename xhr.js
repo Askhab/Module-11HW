e66f446c1e5d470d26463bfb4420e29252eacb88
Askhab <asmag86@gmail.com>
2021-04-02 15:58:36 +0300

    Numbers of all requesting objects

--- xhr.js
+++ xhr.js
@@ -15,14 +15,35 @@ let object = document.querySelector(".person_search"),
 searchButton.addEventListener("click", requestData);
 
 function requestData() {
-  const xhr = new XMLHttpRequest(),
-        url = api + objectType.value;
+  const xhr = new XMLHttpRequest();
+  let url = api + objectType.value;
+
+  if(objectType.value === 'people') {
+    url += '/82/';
+  } else if(objectType.value === 'starships') {
+    url += '/36/';
+  } else if(objectType.value === 'planets') {
+    url += '/60/';
+  } else if(objectType.value === 'films') {
+    url += '/6/';
+  } else if(objectType.value === 'vehicles') {
+    url += '/39/';
+  }
 
   xhr.open("GET", url);
   xhr.responseType = "json";
   xhr.send();
   xhr.onload = () => {
-    let object = xhr.response;
+    let object = xhr.response.results,
+        listItem = document.createElement('li');
     console.log(object);
+    console.log(object.length);
+
+    for(let i of object) {
+      if(object.length) {
+        listItem.textContent = i.name;
+        findedObjects.append(listItem);
+      }
+    }
   };
 }
