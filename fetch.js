364824108b6e4fa0e3e4ce04a27b04114f9a711f
Askhab <asmag86@gmail.com>
2021-04-08 16:52:33 +0300

    All dane ... maybe

--- fetch.js
+++ fetch.js
@@ -1,29 +1,65 @@
-// "use strict";
+"use strict";
 
-// document.onload = function() {
-//     const api = "https://swapi.dev/api",
-//           searchButton = document.querySelector('#search_request_btn');
+const api = "https://swapi.dev/api/",
+      searchButton = document.querySelector("#search_request_btn");
 
-//     let object = document.querySelector('.person_search'),
-//         objectType = document.querySelector('option[selected]'),
-//         findedObjects = document.querySelector('.search_result'),
-//         nameOutput = document.querySelector('#name'),
-//         heightOutput = document.querySelector('#height'),
-//         massOutput = document.querySelector('#mass'),
-//         birthYearOutput = document.querySelector('#birth_year'),
-//         filmsCountOutput = document.querySelector('#films_count'),
-//         xhrRequest = new XMLHttpRequest();
+let object = document.querySelector(".person_search"),
+    answer = [],
+    objectType = document.querySelector(".search_type"),
+    findedObjects = document.querySelector(".search_result"),
+    nameOutput = document.querySelector("#name"),
+    heightOutput = document.querySelector("#height"),
+    massOutput = document.querySelector("#mass"),
+    birthYearOutput = document.querySelector("#birth_year"),
+    filmsCountOutput = document.querySelector("#films_count");
 
-    
-//     searchButton.addEventListener('click', );
+searchButton.addEventListener("click", requestData);
+findedObjects.addEventListener("click", showObjectData);
 
-    
-//     xhrRequest.open('GET', api, true);
-//     xhrRequest.responseType = 'json';
-//     xhrRequest.onload = () => {
-//         console.log(xhrRequest.response);
-//     };
-//     xhrRequest.send();
+async function requestData() {
+  if (findedObjects.hasChildNodes("li")) {
+        findedObjects.innerHTML = "";
+      }
+  
+  let url = api + objectType.value + "/?search=" + object.value,
+      response = await fetch(url),
+      data = await response.json();
 
+  answer = await data.results;
 
-// };
+  for (let item of answer) {
+    let listItem = document.createElement('li');
+
+    switch (objectType.value) {
+      case "films":
+        listItem.textContent = item.title;
+        findedObjects.append(listItem);
+        break;
+      case "starships":
+      case "vehicles":
+      case "people":
+      case "planets":
+        listItem.textContent = item.name;
+        findedObjects.append(listItem);
+        break;
+    }    
+  }
+      
+}
+
+function showObjectData(event) {
+  let item = event.target.closest('li'),
+      itemText = item.textContent;
+  
+  console.info(answer);
+
+  for(let index of answer) {
+    if(index.name === itemText) {
+      nameOutput.textContent = index.name;
+      heightOutput.textContent = index.height;
+      massOutput.textContent = index.mass;
+      birthYearOutput.textContent = index.birth_year;
+      filmsCountOutput.textContent = index.films.length;
+    }
+  }
+}
